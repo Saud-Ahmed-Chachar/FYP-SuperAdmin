c4cc7035d8f507c4ee4b7ba38ba7ded6378f27a4
Saud Ahmed <113105202+Saud-Ahmed-Chachar@users.noreply.github.com>
2024-05-03 01:15:13 +0500

    Update Login.js

--- super-admin/src/components/Login.js
+++ super-admin/src/components/Login.js
@@ -31,9 +31,14 @@ const Login = () => {
               <div className="text-center">
                 <h1 className="text-2xl font-semibold sm:text-3xl">Super Admin Login</h1>
 
+
+     <label htmlFor="Username" className="block text-sm font-medium text-gray-700">
+      Username
+    </label>
               </div>
               <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                 <input
+                  
                   id="Username"
                   name="Username"
                   type="text"
@@ -43,6 +48,10 @@ const Login = () => {
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
                 />
+
+                        <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+      Password
+    </label>
                 <input
                   id="password"
                   name="password"
